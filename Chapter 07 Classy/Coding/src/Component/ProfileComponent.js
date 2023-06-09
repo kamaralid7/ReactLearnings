f8e31c4c5a77cb8c59420446378d0552e88aebe7
kamaralid7 <126551638+kamaralid7@users.noreply.github.com>
2023-06-09 17:59:09 +0530

    first commit

--- Chapter 07 Classy/Coding/src/Component/ProfileComponent.js
+++ Chapter 07 Classy/Coding/src/Component/ProfileComponent.js
@@ -8,19 +8,39 @@ class ProfileClass extends React.Component{
         super(props)
         this.state = {
             count: 90,
-            count2: 0
+            count2: 0,
+        };
+        this.state = {
+            userInfo: {
+                name: "Dummy",
+                location: "",
+                company: "",
+            }
         };
         console.log("constructor");
     }
 
+    async componentDidMount(){
+
+        const data = await fetch("https://api.github.com/users/kamaralid7");
+        const json = await data.json();
+        this.setState({
+            userInfo: json
+        })
+        console.log(json)
+    }
+
     render(){
+        
+        const { count } = this.state;
         console.log('render() method')
+        console.log(this.state)
         return (
             <div style={ {color:"blueviolet"}}>
                 <h1>React Class Component</h1>
-                <h2>Name : {this.props.Name}</h2>
-                <h2>XYZ : {this.props.xyz}</h2>
-                <h2>Count: {this.state.count}</h2>
+                <h2>Name : {this.state.userInfo.name}</h2>
+                <h2>Location : {this.state.userInfo.location}</h2>
+                <h2>Count: {count}</h2>
                 // Never Mutate State Variables <br />
                 <button onClick={ () => {
                     this.setState({
