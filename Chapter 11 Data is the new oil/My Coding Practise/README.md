0f78b991d9c9d2f78d1a458d119144625d51998c
kamaralid7 <126551638+kamaralid7@users.noreply.github.com>
2023-07-03 20:16:41 +0530

    Lifting the state up

--- Chapter 11 Data is the new oil/My Coding Practise/README.md
+++ Chapter 11 Data is the new oil/My Coding Practise/README.md
@@ -11,3 +11,4 @@ AppLayout.
 Prop drilling occurs when a parent component passes data down to its children and then those children pass the same data down to their own children. This process can continue indefinitely. At the end, it's a long chain of component dependencies that can be difficult to manage and maintain.     
 
 ## Lifting the State Up
+In React, sharing state is accomplished by moving it up to the closest common ancestor of the components that need it. This is called “lifting state up”.
--- Chapter 11 Data is the new oil/My Coding Practise/src/Component/Instamart.js
+++ Chapter 11 Data is the new oil/My Coding Practise/src/Component/Instamart.js
@@ -1,8 +1,7 @@
 import { useState } from "react";
 
-const Section = ({title, description }) => {
+const Section = ({title, description, isVisible, setIsVisible }) => {
 
-    const [isVisible, setIsVisible] = useState(false)
     return(
         <div className=" border-black border-solid border-2 p-2 m-2  "> 
             <h2 className="text-xl font-bold ">
@@ -27,19 +26,42 @@ const Section = ({title, description }) => {
     );
 }
 const Instamart = () => {  
+    const [sectionConfig, setSectionConfig] = useState({
+        showAbout: true,
+        showTeam: false,
+        showCareer: true
+    })    
      return (
          <div>
              <h1 className="text-2xl font-bold ">Instamart</h1>
             <Section title = {"About Instamart"} 
             description = {"What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."}            
+            isVisible={sectionConfig.showAbout}
+            setIsVisible = { () => setSectionConfig({
+                showAbout: true,
+                showTeam: false,
+                showCareer: false
+            })}
             />    
             
             <Section title = {"Team Instamart"} 
             description = {"What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."}            
+            isVisible={sectionConfig.showTeam}
+            setIsVisible = { () => setSectionConfig({
+                showAbout: false,
+                showTeam: true,
+                showCareer: false
+            })}
             />       
 
             <Section title = {"Careers"} 
             description = {"What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."}            
+            isVisible={sectionConfig.showCareer}
+            setIsVisible = { () => setSectionConfig({
+                showAbout: false,
+                showTeam: false,
+                showCareer: true
+            })}
             />                
          </div>
      )
